e4a9d77e8496c67f64a32fce81201bb6091edaaa
Ivan Saranin <ivan@saranin.com>
2018-06-07 20:58:20 +0700

    add dockers for different versions, fix gulp to auto build and push

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,6 @@ var childProcess	= require('child_process');
 var runSequence		= require('run-sequence');
 
 // task for bumping version and commit new version
-
 gulp.task('new-version', function() {
 	var importance = '';
 	if (argv.patch) {
@@ -33,36 +32,52 @@ gulp.task('new-version', function() {
 		.pipe(tagVersion());
 });
 
-gulp.task('docker-push', function() {
+// task for building docker images
+gulp.task('docker:build', function() {
 	var json = JSON.parse(fs.readFileSync('./package.json')),
-		versions = json['version'].split('.').unshift('latest'),
+		versions = json['version'].split('.'),
 		version = '',
+		// using repo and docker-name to make full path
 		dockerName = json['repository-docker']+json['docker-name'],
-		dockers = ['php5.6', 'php7.1', 'php7.2'];
+		// getting all dockers file to build, the last (alphabet) will be last,
+		// and used for versions tags
+		dockers = fs.readdirSync('dockers').sort();
 
 	// building and pushing docker images for every php version
 	var dockerImageName = '',
 		dockerFilename = '';
+
+	// building for every file in dockers folder
 	dockers.forEach(function(phpVersion) {
 		dockerFilename = 'dockers/'+phpVersion;
 		dockerImageName = dockerName+':'+phpVersion;
 		log('Building docker image "'+dockerImageName+'"...');
-		childProcess.execSync('docker build -f '+dockerFilename+' -t '+dockerName+' .', log.error);
-		log('Pushing docker image "'+dockerImageName+'"...');
-		childProcess.execSync('docker push '+dockerImageName, log.error);
+		childProcess.execSync('docker build -f '+dockerFilename+' -t '+dockerImageName+' .', log.error);
 	});
 
-	// using last docker image name as base image for latest and versions images
+	// using last docker image name as base image for versions images
 	versions.forEach(function(value) {
 		version += ((version === '')?'':'.')+value;
 		var newDockerImageName = dockerName+':'+version;
-		log('Tag and pushing docker image "'+newDockerImageName+'"...');
+		log('Tagging docker image "'+dockerImageName+'" to "'+newDockerImageName+'"...');
 		childProcess.execSync('docker tag '+dockerImageName+' '+newDockerImageName, log.error);
-		childProcess.execSync('docker push '+newDockerImageName, log.error);
 	});
 
+	// adding latest tag
+	var newDockerImageName = dockerName + ':' + 'latest';
+	log('Tagging docker image "'+dockerImageName+'" to "'+newDockerImageName+'"...');
+	childProcess.execSync('docker tag '+dockerImageName+' '+newDockerImageName, log.error);
 });
 
+// task for pushing docker images
+gulp.task('docker:push', function() {
+	var json = JSON.parse(fs.readFileSync('./package.json')),
+		dockerName = json['repository-docker']+json['docker-name'];
+
+	childProcess.execSync('docker push '+dockerName, log.error);
+});
+
+// task adding new version, building docker images and then pushes them
 gulp.task('deploy', function() {
-	runSequence('new-version','docker-push');
+	runSequence('new-version', 'docker:build', 'docker:push');
 });
